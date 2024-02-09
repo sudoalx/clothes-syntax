71438c5c2cd994275b4daff208f2a44b546f612b
Alex <alex@alex-dev.me>
2024-02-09 02:58:42 -0600

    Add image hover effect to ProductGridItem

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,19 +1,26 @@
+"use client";
 import { Product } from "@/interfaces";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 interface Props {
   product: Product;
 }
 
 export const ProductGridItem = ({ product }: Props) => {
+  const [displayImage, setDisplayImage] = useState(product.images[0]);
+
   return (
     <div key={product.slug} className="rounded-md overflow-hidden fade-in">
       <Image
         width={300}
         height={300}
-        src={`/products/${product.images[0]}`}
+        src={`/products/${displayImage}`}
         alt={product.title}
+        onMouseEnter={() => setDisplayImage(product.images[1])}
+        onMouseLeave={() => setDisplayImage(product.images[0])}
+        className="cursor-pointer transition-all duration-300 ease-in-out hover:scale-105 transform-gpu"
       />
       <div className="p-4 flex flex-col">
         <Link
