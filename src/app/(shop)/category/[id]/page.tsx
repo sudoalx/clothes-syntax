672dd4a46618964341c0e03349d96d16ac914bb8
Alex <alex@alex-dev.me>
2024-02-09 02:02:30 -0600

    Refactor category page component

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,25 +1,23 @@
-import { notFound } from 'next/navigation';
-
+import { notFound } from "next/navigation";
 
 interface Props {
   params: {
     id: string;
-  }
+  };
 }
 
-
-export default function({ params }: Props) {
-
+export default function ({ params }: Props) {
   const { id } = params;
 
-  if ( id === 'kids' ) {
-    notFound();
+  const allowedCategories = ["men", "women", "kids"];
+  if (!allowedCategories.includes(id)) {
+    return notFound();
   }
 
-
+  const name = id.charAt(0).toUpperCase() + id.slice(1);
   return (
     <div>
-      <h1>Category Page { id }</h1>
+      <h1>{name} category</h1>
     </div>
   );
 }
