dc9a4e2e2d94adb103f601e753ec0f30a24e85c1
Alex <alex@alex-dev.me>
2024-02-08 23:06:18 -0600

    Refactor AuthLayout props

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,8 +1,8 @@
 export default function AuthLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <main className="bg-gray-800 flex min-h-screen text-gray-50">
       {children}
