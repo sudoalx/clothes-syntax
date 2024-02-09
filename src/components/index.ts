c811ad965051598d99ed872e8f97bec387c490ab
Alex <alex@alex-dev.me>
2024-02-09 02:50:09 -0600

    Add new product grid components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
+export { ProductGrid } from "./products/product-grid/ProductGrid";
+export { ProductGridItem } from "./products/product-grid/ProductGridItem";
+
 export { TopMenu } from "./ui/top-menu/TopMenu";
 export { PageNotFound } from "./ui/not-found/PageNotFound";
 export { Title } from "./ui/title/Title";
