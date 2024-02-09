977181fbc6597b2e3f4534cc817838bafff0c8a3
Alex <alex@alex-dev.me>
2024-02-08 23:03:55 -0600

    Renamed project: package.json and layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 import { inter } from "@/config/fonts";
 
 export const metadata: Metadata = {
-  title: "Fashion Syntax",
-  description: "A fashion store for developers",
+  title: "Clothes Syntax",
+  description: "A clothing store for developers;",
 };
 
 export default function RootLayout({
