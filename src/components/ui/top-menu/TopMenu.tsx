65786b645b325d2a759476cd2d7b036b330d401d
Alex <alex@alex-dev.me>
2024-02-09 01:56:04 -0600

    Add menu button to top menu

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -52,6 +52,25 @@ export const TopMenu = () => {
           </div>
           <IoCartOutline className="w-5 h-5" />
         </Link>
+        <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">
+          <span className="sr-only">Open Menu</span>
+          <span aria-hidden="true">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </span>
+        </button>
       </div>
     </nav>
   );
