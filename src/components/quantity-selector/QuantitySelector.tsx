0312db9f7903001836ccbbac969d5828081f08a2
Alex <alex@alex-dev.me>
2024-02-09 18:09:01 -0600

    Refactor quantity selector component to allow dynamic quantity changes

--- src/components/quantity-selector/QuantitySelector.tsx
+++ src/components/quantity-selector/QuantitySelector.tsx
@@ -1,17 +1,33 @@
+"use client";
+
+import { useState } from "react";
+
 interface Props {
   quantity: number;
 }
 
 export const QuantitySelector = ({ quantity }: Props) => {
+  const [QtyCount, setQtyCount] = useState(quantity);
+
+  const onQtyChange = (value: number) => {
+    if (QtyCount + value < 1 || QtyCount + value > 5) return;
+    setQtyCount(QtyCount + value);
+  };
   return (
     <div className="my-5">
       <p className="text-gray-500 mb-5">Quantity</p>
       <div className="flex items-center gap-3 mb-5">
-        <button className="text-gray-500 border border-gray-300 px-3 py-1">
+        <button
+          onClick={() => onQtyChange(-1)}
+          className="text-gray-500 border border-gray-300 px-3 py-1"
+        >
           -
         </button>
-        <span className="text-gray-500">1</span>
-        <button className="text-gray-500 border border-gray-300 px-3 py-1">
+        <span className="text-gray-500">{QtyCount}</span>
+        <button
+          onClick={() => onQtyChange(1)}
+          className="text-gray-500 border border-gray-300 px-3 py-1"
+        >
           +
         </button>
       </div>
