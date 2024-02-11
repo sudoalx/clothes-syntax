a1bbb07f3632dcd5e3f71b88361a2d9029635872
Alex <alex@alex-dev.me>
2024-02-11 00:18:26 -0600

    Refactor empty page component to display a cart icon and link to the shop

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+import { IoCartOutline } from "react-icons/io5";
+
 export default function EmptyPage() {
   return (
-    <div>
-      <h1>Empty Page</h1>
+    <div className="flex justify-center items-center h-screen">
+      <IoCartOutline size={80} className="text-6xl  mx-5" />
+      <div className="flex flex-col items-center">
+        <h1 className="font-semibold">Your cart is empty</h1>
+        <Link href={"/"} className="text-blue-500 hover:underline mt-2">
+          Return to shop
+        </Link>
+      </div>
     </div>
   );
 }
