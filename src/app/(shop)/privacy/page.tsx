30f457161ede0bd5c2bba95d919ba6538ef72a5c
Alex <alex@alex-dev.me>
2024-02-11 01:32:34 -0600

    Add terms of service page

---
+++ src/app/(shop)/privacy/page.tsx
@@ -0,0 +1,213 @@
+import { Title } from "@/components";
+
+export default function PrivacyPage() {
+  return (
+    <>
+      <Title>Privacy Policy</Title>
+      <h3 className="text-2xl font-bold">1. Introduction</h3>
+      <p>
+        <span>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos
+          adipisci ex fuga laudantium magnam beatae! Ipsa iusto placeat dolores!
+          Architecto natus aperiam, iure voluptate totam odit quos! At,
+          necessitatibus laborum?
+        </span>
+        <span>
+          Facilis, excepturi? Omnis quisquam aliquid laboriosam. Laudantium
+          minima dignissimos quia, optio perferendis sed ipsum commodi itaque a
+          sequi exercitationem, quibusdam amet sint quos voluptatem laborum,
+          accusantium voluptatum laboriosam doloremque ad!
+        </span>
+        <span>
+          Reiciendis, sequi ipsa! Eos hic fugiat optio in reprehenderit
+          explicabo impedit obcaecati porro. Ut pariatur in dolorem a provident
+          obcaecati quam doloribus eos harum dolor, nam, maiores inventore
+          assumenda nostrum?
+        </span>
+        <span>
+          Facere sapiente magnam voluptatem a eaque nihil numquam, quas harum id
+          eius explicabo recusandae veritatis quae quam ex assumenda blanditiis
+          nobis consequatur, unde quidem commodi cum vel similique! Praesentium,
+          dignissimos!
+        </span>
+        <span>
+          Corrupti illo nihil quia sint dolore pariatur ratione placeat
+          perferendis recusandae porro quaerat minus voluptatibus ipsum,
+          deserunt sunt ipsam officiis distinctio vero ipsa perspiciatis
+          consectetur? Quidem eaque dolore eos doloremque!
+        </span>
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">2. What data do we collect?</h3>
+      <p>
+        <span>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem optio
+          asperiores repellat illo consequatur ducimus earum voluptatibus. Ipsam
+          eaque corporis incidunt, suscipit, facere vero iure accusantium a
+          voluptatibus minima sint?
+        </span>
+        <span>
+          Eius dolorum omnis magnam quod autem ratione architecto ea ipsa
+          officia dolore culpa hic, consequatur laudantium incidunt ad eligendi.
+          Magni, sit modi id iusto laudantium debitis nesciunt fugiat delectus.
+          In.
+        </span>
+        <span>
+          Assumenda neque molestias unde necessitatibus deserunt at commodi,
+          doloremque sit fuga alias labore vitae ad! Modi qui voluptates fugiat
+          officiis, harum corrupti itaque delectus autem debitis quidem iusto
+          cum perferendis!
+        </span>
+        <span>
+          Repellat earum dolorum deleniti suscipit! Temporibus incidunt quaerat
+          similique atque sint non, pariatur asperiores tempore assumenda
+          ducimus deleniti, reprehenderit quasi sit quia cum vel distinctio
+          voluptatem possimus nam fugit exercitationem.
+        </span>
+        <span>
+          Veniam voluptas dignissimos molestiae repellat voluptates, aspernatur
+          quod qui quae nihil sunt. Dicta neque, eligendi alias voluptatum modi
+          amet fuga incidunt ducimus, quisquam voluptatibus veniam, aperiam
+          velit libero animi voluptas!
+        </span>
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">3. How do we use your data?</h3>
+      <p>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, fugit
+          ratione. Similique ipsa quos minus fugiat, a facilis corrupti nam
+          tenetur, excepturi doloribus sit harum neque qui reprehenderit, ex
+          accusamus?
+        </span>
+        <span>
+          Quod beatae, eligendi obcaecati saepe culpa molestiae sunt numquam
+          modi mollitia, quasi ea necessitatibus neque laborum sit atque
+          voluptate? Sit repellendus amet culpa exercitationem cupiditate
+          voluptates adipisci nostrum a iste!
+        </span>
+        <span>
+          Dolore minus vel facere consequatur dignissimos vero veniam a quas
+          harum, repellat illo, at, nam voluptas! Deserunt, exercitationem
+          ducimus? Voluptatum unde iste delectus ea a accusamus exercitationem
+          labore aliquam animi.
+        </span>
+        <span>
+          Amet totam architecto pariatur, perspiciatis minima quam sit ipsa
+          rerum exercitationem facere at atque soluta voluptatum, voluptates
+          repellendus quibusdam odio aliquam recusandae nam ratione natus quia
+          minus enim eligendi. Ipsam?
+        </span>
+        <span>
+          Perferendis odio minus quasi excepturi suscipit tempore sapiente iste
+          dignissimos temporibus aliquam quisquam alias ducimus, porro
+          cupiditate magni nihil, libero deleniti doloribus voluptates dicta
+          blanditiis molestias iure nesciunt. Aperiam, ducimus.
+        </span>
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">4. How do we store your data?</h3>
+      <p>
+        <span>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
+          reprehenderit odit suscipit quibusdam expedita nam voluptate eligendi
+          facere illum eos, est sapiente quas libero dolores aut aliquam ex cum!
+          Alias?
+        </span>
+        <span>
+          Commodi veritatis porro in autem incidunt aperiam voluptatum assumenda
+          natus similique! Doloribus at eius amet dolores quod non nostrum,
+          asperiores blanditiis nihil illum officia perferendis debitis aperiam
+          neque sapiente architecto.
+        </span>
+        <span>
+          Quos ad iusto similique debitis laboriosam fugit adipisci, possimus ut
+          ab culpa perspiciatis aliquid ullam dolore temporibus distinctio
+          dolorum! Deleniti, aliquid quam. Quidem veniam, at fugiat eligendi
+          saepe recusandae hic.
+        </span>
+        <span>
+          Minima quis dolorum culpa voluptatem veritatis. Ipsam nobis placeat
+          aliquid dolores accusamus numquam, cumque expedita nemo atque enim ea
+          reprehenderit delectus aut et, sit dolor veritatis ut suscipit
+          voluptatibus saepe.
+        </span>
+        <span>
+          Alias, perspiciatis. Aliquid vitae quia fuga beatae voluptas deleniti!
+          Natus placeat cumque assumenda nesciunt vel. At quos molestias enim
+          corrupti numquam voluptatum ipsam voluptates sint commodi mollitia,
+          necessitatibus laboriosam sequi.
+        </span>
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">5. Marketing</h3>
+      <p>
+        <span>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam
+          soluta quasi quis, voluptates corrupti animi, tenetur minima atque,
+          explicabo praesentium deserunt. Consequuntur doloremque totam repellat
+          nulla sunt cupiditate iure dolorem?
+        </span>
+        <span>
+          Totam excepturi molestiae aspernatur itaque aperiam optio qui. Cumque
+          vitae laborum, asperiores aliquam reiciendis impedit dignissimos
+          explicabo modi. Unde consectetur fugiat, quam consequatur nisi ratione
+          ducimus cum similique at? Nesciunt.
+        </span>
+        <span>
+          Illum possimus laboriosam non nemo adipisci quidem autem mollitia
+          dicta maiores sed recusandae accusamus aspernatur a qui ad corporis,
+          commodi maxime ut nam quod perferendis id? Qui dolorum ab aperiam!
+        </span>
+        <span>
+          Omnis quidem amet reprehenderit officiis sequi quod velit sunt dolor
+          quos placeat vel, modi nihil esse? Deleniti voluptates praesentium
+          distinctio, quo natus culpa accusantium quam eos tenetur soluta
+          exercitationem aut!
+        </span>
+        <span>
+          Reiciendis, inventore. Nemo velit, ex, soluta numquam inventore libero
+          officia fuga similique accusantium, repudiandae repellendus quisquam
+          amet sit praesentium fugiat nostrum non! Et repellat amet nemo,
+          corrupti totam velit quasi.
+        </span>
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">
+        6. What are your data protection rights?
+      </h3>
+      <p>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ad
+          natus ex maxime saepe non magnam amet, corporis porro repellat
+          molestiae laborum quisquam, modi suscipit expedita soluta reiciendis
+          quam enim?
+        </span>
+        <span>
+          Rerum esse facilis assumenda. Nam laborum labore dolor nihil dicta
+          blanditiis quam ipsum molestias magnam exercitationem doloremque, modi
+          repellat, expedita molestiae corrupti vel nulla illo soluta. Explicabo
+          dolores id ipsa.
+        </span>
+        <span>
+          Deserunt, totam illum? Molestias laboriosam nam distinctio doloremque
+          asperiores debitis natus nihil culpa soluta, sit inventore iure ut
+          quidem modi voluptatum, numquam quis unde error rerum est et, sunt
+          molestiae!
+        </span>
+        <span>
+          Eaque, sit rerum. Autem in voluptate magni veritatis recusandae,
+          consequuntur nam commodi ducimus adipisci labore doloribus
+          exercitationem quia aliquid placeat, hic impedit nisi cum obcaecati
+          dicta reiciendis. Sit, nesciunt dolores!
+        </span>
+        <span>
+          Veritatis possimus deserunt aspernatur ipsam architecto labore
+          repudiandae, voluptatum esse minima? At labore optio delectus quas,
+          illum suscipit quaerat vero eum repellat repudiandae voluptate magnam
+          dolores voluptatum, culpa alias modi.
+        </span>
+      </p>
+    </>
+  );
+}
---
+++ src/app/(shop)/terms/page.tsx
@@ -0,0 +1,174 @@
+import { Title } from "@/components";
+
+export default function TermsPage() {
+  return (
+    <>
+      <Title>Terms of Service</Title>
+      <h3 className="text-2xl font-bold">1. Introduction</h3>
+
+      <p>
+        <span>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias
+          eum placeat quae vero maxime, libero velit perspiciatis aspernatur
+          sequi quaerat iste suscipit ullam est nobis sapiente, dignissimos aut
+          ipsum delectus.
+        </span>
+        <span>
+          Asperiores nobis at officiis quas quaerat ipsam autem excepturi illum,
+          fuga blanditiis placeat optio voluptate amet molestias, iure aperiam
+          corporis labore cumque veniam deleniti aspernatur accusantium
+          distinctio suscipit dolorum. Necessitatibus.
+        </span>
+        <span>
+          Hic sint eius earum amet nobis eaque nisi sequi saepe alias corporis
+          ullam quo, illo tempore sunt possimus? Rerum rem voluptate et ut
+          repudiandae dicta fugiat consequatur ad pariatur amet!
+        </span>
+        <span>
+          Laudantium labore tempora id dolor quod dolore cumque, rerum excepturi
+          sit vel, blanditiis eos asperiores libero? Voluptates facilis
+          architecto culpa odio soluta deserunt, omnis cum commodi, sapiente
+          ipsa laborum harum!
+        </span>
+        <span>
+          Consequatur non quod iure ipsam odit, pariatur ratione ad libero natus
+          aliquid a nulla eius eum totam iste cumque veritatis cupiditate
+          laboriosam quis voluptatibus nemo ea accusantium voluptatum enim!
+          Doloremque.
+        </span>
+      </p>
+      <h3 className="text-2xl font-bold">2. What are the terms?</h3>
+      <p>
+        <span>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta
+          distinctio esse placeat saepe perspiciatis omnis, ea, modi quod
+          eveniet beatae quaerat eius sed aspernatur, voluptate doloremque amet.
+          Ducimus, earum unde!
+        </span>
+        <span>
+          Harum delectus necessitatibus explicabo, minus dignissimos officiis
+          obcaecati error facere, voluptatem officia quos odio est laudantium a
+          suscipit nam ducimus inventore, quidem maxime eaque aliquid molestiae
+          deleniti corrupti. Accusantium, ex.
+        </span>
+        <span>
+          Laborum perferendis corrupti accusamus obcaecati iure molestiae velit
+          ullam ex? Distinctio blanditiis maxime molestias illum vel, placeat
+          culpa ad sapiente doloribus porro natus eum ut alias fugiat. Est, ad
+          dolorum.
+        </span>
+        <span>
+          Reprehenderit nostrum iste ducimus tenetur nobis, laboriosam
+          distinctio harum totam, tempora quasi similique repellendus, at
+          laudantium recusandae animi quae voluptatibus labore ipsam ea omnis?
+          Ipsum facere ratione pariatur adipisci a?
+        </span>
+        <span>
+          Tenetur nobis voluptatibus illum reprehenderit eveniet sit, doloribus
+          quis minima dolore nisi perspiciatis ex tempore. Aliquid ut laboriosam
+          ea labore doloribus veniam, officiis quos quas illum quibusdam
+          obcaecati amet inventore?
+        </span>
+      </p>
+      <h3 className="text-2xl font-bold">3. What are the terms?</h3>
+      <p>
+        <span>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex, dolor
+          officia tempora error modi quidem voluptas repellendus quos, sunt
+          praesentium obcaecati nisi quod quas neque aliquid natus? Neque,
+          soluta quae!
+        </span>
+        <span>
+          Dolor iusto dolore nostrum ea deleniti expedita. Commodi tempore
+          libero, fuga est praesentium a asperiores, qui iste ex voluptate
+          numquam dolorem repellat recusandae incidunt. Fugiat accusantium
+          laudantium voluptas aut corporis.
+        </span>
+        <span>
+          Ducimus natus tempore expedita voluptatum eaque distinctio,
+          reiciendis, maxime ea rerum earum sint iusto? Inventore accusantium ex
+          consequatur quod provident non repudiandae iure eligendi vitae, unde
+          vel voluptate! Nemo, suscipit!
+        </span>
+        <span>
+          Voluptate, et quas quaerat odit commodi quam praesentium, repellendus
+          provident nemo rerum quae nesciunt, architecto temporibus? Tempore,
+          quae vero soluta sint odit quibusdam a aut eaque, vitae, voluptatem
+          iure voluptatibus.
+        </span>
+        <span>
+          Qui dolores, nam maiores commodi expedita nemo, dolorem consequatur
+          quam earum possimus, corporis reprehenderit repellendus veritatis quas
+          dignissimos temporibus quod. Molestias, voluptatem dolor. Minus
+          aperiam ullam totam voluptates reprehenderit est?
+        </span>
+      </p>
+      <h3 className="text-2xl font-bold">4. What are the terms?</h3>
+      <p>
+        <span>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio minus,
+          earum quidem dolor, perferendis deleniti inventore veritatis itaque
+          quasi temporibus illo praesentium, asperiores rerum animi hic maiores
+          similique cupiditate voluptate.
+        </span>
+        <span>
+          Quo nam labore consequatur architecto temporibus sunt aperiam modi
+          tenetur veniam praesentium impedit quia laudantium, ratione porro
+          autem laboriosam laborum quasi! Iure inventore, velit earum odio
+          impedit architecto! Perspiciatis, cum.
+        </span>
+        <span>
+          Sint vel amet ipsum aliquid quos! Quam, maxime laborum libero vitae
+          fuga nisi quidem accusamus ratione repellat! Minus quae cupiditate
+          ipsam doloribus earum, atque incidunt soluta quam. Quae, aperiam
+          adipisci.
+        </span>
+        <span>
+          Odit, molestiae nostrum corporis itaque beatae adipisci soluta libero
+          tempora, pariatur id, sapiente voluptates perspiciatis sunt. Fugit
+          dolore sequi magnam tenetur rem, esse vero vel maiores quidem dolor
+          eos nam.
+        </span>
+        <span>
+          Praesentium, est, quisquam atque nihil nemo quis delectus facilis, aut
+          ea error sapiente omnis voluptate! Sequi quam, nesciunt nemo repellat
+          optio itaque, quas illo quisquam debitis error architecto, perferendis
+          corporis.
+        </span>
+      </p>
+      <h3 className="text-2xl font-bold">5. What are the terms?</h3>
+      <p>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          voluptas iure officiis, adipisci unde numquam recusandae ex repellat
+          esse ipsum non modi fugit deserunt! Temporibus placeat asperiores
+          doloremque eaque impedit?
+        </span>
+        <span>
+          Maiores libero suscipit, in sed, tempore atque, ratione consectetur
+          non quidem distinctio deserunt iure quod. Ratione, minima enim
+          consectetur maxime non odit ab repellendus nesciunt inventore. Atque
+          sunt laudantium doloribus.
+        </span>
+        <span>
+          Omnis necessitatibus quam impedit ratione natus voluptas architecto
+          voluptatum voluptatem molestiae odit. Dicta ab autem officiis beatae
+          pariatur architecto qui eum? Quia, voluptatem amet nostrum deserunt
+          vitae nam incidunt optio?
+        </span>
+        <span>
+          Maxime, accusantium. Aliquid consequuntur quis illum natus eum fugit
+          architecto perferendis neque vero voluptatibus est illo dolorem quas
+          ipsum, at laudantium, ullam placeat recusandae? Porro at amet cumque a
+          saepe!
+        </span>
+        <span>
+          Ipsum natus neque dolore perferendis facilis ex iusto. Et, delectus.
+          Dignissimos voluptatem temporibus nulla nihil? Dignissimos commodi
+          voluptate exercitationem voluptatum? Voluptatibus nesciunt doloribus
+          quasi veniam dolorem earum, beatae nulla iusto.
+        </span>
+      </p>
+    </>
+  );
+}
