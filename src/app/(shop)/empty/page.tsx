d358fc30b334b5a628510099a24c9e74a7577374
Alex <alex@alex-dev.me>
2024-02-11 00:38:13 -0600

    Update empty cart page layout

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -3,9 +3,9 @@ import { IoCartOutline } from "react-icons/io5";
 
 export default function EmptyPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <IoCartOutline size={80} className="text-6xl  mx-5" />
-      <div className="flex flex-col items-center">
+    <div className="flex justify-center items-center h-[calc(100vh-14rem)] md:text-4xl">
+      <IoCartOutline size={120} className="mx-5" />
+      <div className="flex flex-col">
         <h1 className="font-semibold">Your cart is empty</h1>
         <Link href={"/"} className="text-blue-500 hover:underline mt-2">
           Return to shop
