7d0a2cf11187627a19b12107fe1257d95af4fa7c
Alex <alex@alex-dev.me>
2024-02-10 20:47:14 -0600

    Update checkout page with shipping address and order summary

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -27,7 +27,7 @@ export default function CheckoutPage() {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
           {/* Cart */}
           <div className="flex flex-col mt-5">
-            <span className="text-xl">Confirm your cart</span>
+            <span className="text-xl">Confirm your order</span>
             <Link
               href={"/cart"}
               className="underline text-blue-500 hover:text-blue-600"
@@ -77,54 +77,93 @@ export default function CheckoutPage() {
           <div className="flex flex-col mt-5">
             <div className="bg-white shadow-xl p-7 rounded-xl">
               <div>
-                <h2 className="text-2xl mb-2">Order summary</h2>
+                {/* Display shipping address */}
+                <div className="">
+                  <h2 className="text-2xl mb-2">Shipping address</h2>
+                  <div className="mb-10">
+                    <span>
+                      Name: <span className="font-bold">John Doe</span>
+                    </span>
+                    <br />
+                    <span>
+                      Address: <span className="font-bold">1234 Main St.</span>
+                    </span>
+                    <br />
+                    <span>
+                      Address 2:{" "}
+                      <span className="font-bold">
+                        Apartment, studio, or floor
+                      </span>
+                    </span>
+                    <br />
+                    <span>
+                      Zip Code: <span className="font-bold">12345-6789</span>
+                    </span>
+                    <br />
+                    <span>
+                      City: <span className="font-bold">New York</span>
+                    </span>
+                    <br />
+                    <span>
+                      Country: <span className="font-bold">United States</span>
+                    </span>
+                    <br />
+                    <span>
+                      Phone: <span className="font-bold">123-456-7890</span>
+                    </span>
+                  </div>
+                </div>
+                <hr />
+                <div className="mt-10">
+                  <h2 className="text-2xl mb-2">Order summary</h2>
 
-                <div className="grid grid-cols-2">
-                  <span>Products</span>
-                  <span className="text-right">
-                    {productsInCart.length} items
-                  </span>
-                  <span>Subtotal</span>
-                  <span className="text-right">
-                    $
-                    {productsInCart.reduce(
-                      (acc, product) => acc + product.price,
-                      0
-                    )}
-                  </span>
-                  <span>
-                    Taxes <span className="text-sm">(15%)</span>
-                  </span>
-                  <span className="text-right">
-                    $
-                    {productsInCart.reduce(
-                      (acc, product) => acc + product.price,
-                      0
-                    ) * 0.15}
-                  </span>
-                  <span className="text-2xl mt-5">
-                    Total <span className="text-sm">(USD)</span>
-                  </span>
-                  <span className="text-right text-2xl mt-5">
-                    $
-                    {productsInCart.reduce(
-                      (acc, product) => acc + product.price,
-                      0
-                    ) +
-                      productsInCart.reduce(
+                  <div className="grid grid-cols-2">
+                    <span>Products</span>
+                    <span className="text-right">
+                      {productsInCart.length} items
+                    </span>
+                    <span>Subtotal</span>
+                    <span className="text-right">
+                      $
+                      {productsInCart.reduce(
                         (acc, product) => acc + product.price,
                         0
-                      ) *
-                        0.15}
-                  </span>
+                      )}
+                    </span>
+                    <span>
+                      Taxes <span className="text-sm">(15%)</span>
+                    </span>
+                    <span className="text-right">
+                      $
+                      {productsInCart.reduce(
+                        (acc, product) => acc + product.price,
+                        0
+                      ) * 0.15}
+                    </span>
+                    <span className="text-2xl mt-5">
+                      Total <span className="text-sm">(USD)</span>
+                    </span>
+                    <span className="text-right text-2xl mt-5">
+                      $
+                      {productsInCart.reduce(
+                        (acc, product) => acc + product.price,
+                        0
+                      ) +
+                        productsInCart.reduce(
+                          (acc, product) => acc + product.price,
+                          0
+                        ) *
+                          0.15}
+                    </span>
+                  </div>
                 </div>
               </div>
               <div className="mt-5 mb-2 w-full flex text-center">
                 <Link
-                  href={"/checkout/address"}
+                  href={"#"}
                   className="w-full bg-blue-500 text-white py-3 px-5 rounded-lg hover:bg-blue-600 transition-all"
                 >
-                  Checkout
+                  Confirm order
                 </Link>
               </div>
             </div>
