3f592fb82e7df649f051674b4ff8c656d966cb53
Alex <alex@alex-dev.me>
2024-02-13 16:07:21 -0600

    Add category and product seeding logic

--- src/seed/seed-db.ts
+++ src/seed/seed-db.ts
@@ -9,12 +9,34 @@ async function main() {
     prisma.category.deleteMany(),
   ]);
 
-  const { categories } = initialData;
-
+  // Get the initial data
+  const { categories, products } = initialData;
+  // 2. Seed categories
   const categoriesData = categories.map((name) => ({ name }));
 
   await prisma.category.createMany({
     data: categoriesData,
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map; // { "categoryName": "categoryId" }
+  }, {} as Record<string, string>);
+
+  // 3. Seed products
+  products.forEach(async (product) => {
+    const { images, title, type, ...prod } = product; // Extract images, title, type to avoid passing them to the product directly
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...prod, // Spread the rest of the product data
+        name: title, // Rename title to name
+        categoryId: categoriesMap[type], // Map the category name to the category id
+      },
+    });
+
+    // 4. Seed product images
+    
   });
 
   console.log("Seed executed!");
