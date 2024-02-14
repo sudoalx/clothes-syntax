3f31090083095cd6edb628a6147ac581173e5dff
Alex <alex@alex-dev.me>
2024-02-14 01:15:41 -0600

    Add product pagination action

---
+++ src/actions/products/product-pagination.ts
@@ -0,0 +1,25 @@
+"use server";
+import prisma from "@/lib/prisma";
+
+export const getPaginatedProductsWithImages = async () => {
+  try {
+    const products = await prisma.product.findMany({
+      include: {
+        ProductImage: {
+          take: 2,
+          select: {
+            url: true,
+          },
+        },
+      },
+    });
+    return {
+      products: products.map((product) => ({
+        ...product,
+        images: product.ProductImage.map((image) => image.url),
+      })),
+    };
+  } catch (error: any) {
+    throw new Error("Error in getPaginatedProductsWithImages:", error);
+  }
+};
