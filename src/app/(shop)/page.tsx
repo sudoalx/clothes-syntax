9e2c7512e8a78f26085a13cf8af2daf4cb237156
Alex <alex@alex-dev.me>
2024-02-14 01:37:03 -0600

    Add searchParams for pagination

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,8 +1,16 @@
 import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
 
-export default async function ShopPage() {
-  const { products } = await getPaginatedProductsWithImages();
+interface Props {
+  searchParams: {
+    page?: number;
+  };
+}
+
+export default async function ShopPage({ searchParams }: Readonly<Props>) {
+  const page = searchParams.page ? Number(searchParams.page) : 1;
+  const { products } = await getPaginatedProductsWithImages({ page });
+
   return (
     <>
       <Title subtitle="All products" className="mb-2">
