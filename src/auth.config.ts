5a333988903f52d8abe7f543f5122d0bff13e926
Alex <alex@alex-dev.me>
2024-02-15 17:31:28 -0600

    Add authentication configuration file

---
+++ src/auth.config.ts
@@ -0,0 +1,32 @@
+import type { NextAuthConfig } from "next-auth";
+import NextAuth from "next-auth";
+import credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+
+export const authConfig: NextAuthConfig = {
+  pages: {
+    signIn: "/auth/login",
+    newUser: "/auth/new-account",
+  },
+  providers: [
+    credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({
+            email: z.string().email(),
+            password: z.string().min(8),
+          })
+          .safeParse(credentials);
+
+        if (!parsedCredentials.success) return null;
+
+        const { email, password } = parsedCredentials.data;
+        console.log({ email, password });
+
+        return null;
+      },
+    }),
+  ],
+};
+
+export const { signIn, signOut, auth } = NextAuth(authConfig);
