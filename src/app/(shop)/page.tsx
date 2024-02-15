998cf1949b8c5d4657e0281798252a201daa0813
Alex <alex@alex-dev.me>
2024-02-15 02:06:45 -0600

    Add metadata for Shop page

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,6 +1,7 @@
 export const revalidate = 60;
 import { getPaginatedProductsWithImages } from "@/actions";
 import { Pagination, ProductGrid, Title } from "@/components";
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
 
 interface Props {
@@ -9,10 +10,22 @@ interface Props {
   };
 }
 
+export const metadata: Metadata = {
+  title: "Shop | Clothes Syntax",
+  description: "A clothing store for developers;",
+  openGraph: {
+    type: "website",
+    url: "https://clothes-syntax.vercel.app/",
+    title: "Shop | Clothes Syntax",
+    description: "A clothing store for developers;",
+    images: [],
+  },
+};
 export default async function ShopPage({ searchParams }: Readonly<Props>) {
   const page = searchParams.page ? Number(searchParams.page) : 1;
-  const { products, currentPage, totalPages } =
-    await getPaginatedProductsWithImages({ page });
+  const { products, totalPages } = await getPaginatedProductsWithImages({
+    page,
+  });
   if (products.length === 0) redirect("/");
 
   return (
