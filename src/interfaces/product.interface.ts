d896a28cc8c828688d9fcc47570c56524516e9ae
Alex <alex@alex-dev.me>
2024-02-15 10:42:43 -0600

    Add CartItem interface and export cart-store

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -12,6 +12,16 @@ export interface Product {
   gender: Category;
 }
 
+export interface CartItem {
+  id: string;
+  slug: string;
+  title: string;
+  price: number;
+  quantity: number;
+  size: Size;
+  image: string;
+}
+
 export type Category = "men" | "women" | "kids" | "unisex";
 export type Size = "XS" | "S" | "M" | "L" | "XL" | "XXL" | "XXXL";
 export type Type = "shirts" | "pants" | "hoodies" | "hats";
