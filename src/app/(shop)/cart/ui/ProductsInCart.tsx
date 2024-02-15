aa8f1ec59cb186196c819aaa004b0578e818602a
Alex <alex@alex-dev.me>
2024-02-15 12:13:42 -0600

    Add ProductsInCart component to display products in the cart

--- src/app/(shop)/cart/ui/ProductsInCart.tsx
+++ src/app/(shop)/cart/ui/ProductsInCart.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { QuantitySelector } from "@/components";
+import { useCartStore } from "@/store";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export const ProductsInCart = () => {
   const [loaded, setLoaded] = useState(false);
   const productsInCart = useCartStore((state) => state.cart);
 
@@ -8,6 +17,49 @@
   if (!loaded) {
     return <LoadingSkeleton />;
   }
+  return (
+    <div className="flex flex-col gap-5 mt-5">
+      {productsInCart.map((product) => (
+        <div key={`${product.slug}-${product.size}`} className="flex flex-col">
+          <div className="flex justify-between items-center">
+            <div className="flex gap-3">
+              <Image
+                width={240}
+                height={240}
+                src={`/products/${product.image}`}
+                alt={product.title}
+                className="w-32 h-32"
+              />
+              <div className="flex flex-col">
+                <Link href={`/product/${product.slug}`}>
+                  <span className="text-blue-500 hover:text-blue-600">
+                    {product.title}
+                  </span>
+                </Link>
+                <span>
+                  Price: <span className="font-bold">${product.price}</span>
+                </span>
+                <span>
+                  Size: <span className="font-bold">{product.size}</span>
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <QuantitySelector
+                quantity={product.quantity}
+                onQuantityChanged={(quantity) => console.log(quantity)}
+              />
+              <button className="text-red-500 hover:text-red-600">
+                Remove
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 const LoadingSkeleton = () => {
   return (
     // Skeleton loader while loading
