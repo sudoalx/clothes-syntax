ea584a5a1569443ba32744db7e651b9fe9a6c001
Alex <alex@alex-dev.me>
2024-02-15 23:06:34 -0600

    Improve user authentication and login functionality and logging

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,6 +2,8 @@ import type { NextAuthConfig } from "next-auth";
 import NextAuth from "next-auth";
 import credentials from "next-auth/providers/credentials";
 import { z } from "zod";
+import prisma from "@/lib/prisma";
+import bcryptjs from "bcryptjs";
 
 export const authConfig: NextAuthConfig = {
   pages: {
@@ -18,11 +20,34 @@ export const authConfig: NextAuthConfig = {
           })
           .safeParse(credentials);
 
-        if (!parsedCredentials.success) return null;
+        if (!parsedCredentials.success) {
+          throw new Error("Invalid credentials format");
+        }
 
         const { email, password } = parsedCredentials.data;
 
-        return null;
+        // Search for the user in the database
+        const user = await prisma.user.findFirst({
+          where: {
+            email: email.toLowerCase(),
+          },
+        });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        // Compare the password
+        if (!bcryptjs.compareSync(password, user.password)) {
+          throw new Error("Incorrect password");
+        }
+
+        // Return the user without the password
+        const { password: _, ...rest } = user;
+
+        console.log("User logged:", rest);
+
+        return rest;
       },
     }),
   ],
