6f8e91bdac33ffc45fdf36e075a08a64d79954f1
Alex <alex@alex-dev.me>
2024-02-16 00:01:55 -0600

    Refactor AuthLayout to redirect authenticated users

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,14 @@
-export default function AuthLayout({
+import { auth } from "@/auth.config";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+  if (session?.user) {
+    redirect("/");
+  }
   return <main className="container sm:w-96 mx-auto p-5">{children}</main>;
 }
