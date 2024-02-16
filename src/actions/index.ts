6005adfe53059a4d92bf493b8637aabd14a70c78
Alex <alex@alex-dev.me>
2024-02-16 00:13:21 -0600

    Add logout functionality

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,3 +2,4 @@ export * from "./products/get-product-by-slug";
 export * from "./products/get-stock-by-slug";
 export * from "./products/product-pagination";
 export * from "./auth/login";
+export * from "./auth/logout";
