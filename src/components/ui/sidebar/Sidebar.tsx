8958b0ca2f7855a3fe5f86377ab9933b3342ab86
Alex <alex@alex-dev.me>
2024-02-16 13:15:14 -0600

    Add menu items and categories to the sidebar

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -6,14 +6,17 @@ import {
   IoCloseOutline,
   IoLogInOutline,
   IoLogOutOutline,
+  IoMan,
   IoPeopleOutline,
   IoPersonOutline,
   IoSearchOutline,
   IoShirtOutline,
   IoTicketOutline,
+  IoWoman,
 } from "react-icons/io5";
 import { logout } from "@/actions";
 import { useSession } from "next-auth/react";
+import { TbMoodKidFilled } from "react-icons/tb";
 
 interface MenuItem {
   title: string;
@@ -28,7 +31,25 @@ export const Sidebar = () => {
   const { data: session } = useSession();
 
   const isAuthenticated = !!session?.user;
-  console.log(isAuthenticated, session?.user);
+  const isAdmin = session?.user.role === "ADMIN";
+
+  const menuItems: MenuItem[] = [
+    {
+      title: "Products",
+      url: "#",
+      icon: <IoShirtOutline size={20} className="mx-2 inline-block" />,
+    },
+    {
+      title: "Orders",
+      url: "/orders",
+      icon: <IoTicketOutline size={20} className="mx-2 inline-block" />,
+    },
+    {
+      title: "Users",
+      url: "#",
+      icon: <IoPeopleOutline size={20} className="mx-2 inline-block" />,
+    },
+  ];
 
   return (
     <>
@@ -44,7 +65,7 @@ export const Sidebar = () => {
       {/* sidebar */}
       <nav
         className={clsx(
-          "fixed inset-y-0 right-0 z-50 w-96 max-w-full bg-white h-screen shadow-lg p-4",
+          "fixed inset-y-0 right-0 z-50 w-full sm:w-96 max-w-full bg-white h-screen shadow-lg p-4",
           {
             "translate-x-full": !isSideMenuOpen,
           }
@@ -64,66 +85,90 @@ export const Sidebar = () => {
           />
         </div>
 
-        {/* Menu */}
+        <div className="flex flex-col">
+          <div className="mt-8 sm:hidden">
+            <h3 className="text-xl font-bold mx-2">Categories</h3>
+            <Link
+              href="/profile"
+              className="flex items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
+            >
+              <IoMan size={20} className="mx-2 inline-block" />
+              Men
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
+            >
+              <IoWoman size={20} className="mx-2 inline-block" />
+              Women
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
+            >
+              <TbMoodKidFilled size={20} className="mx-2 inline-block" />
+              Kids
+            </Link>
+            <hr />
+          </div>
 
-        <Link
-          href="/profile"
-          className="flex items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
-        >
-          <IoPersonOutline size={20} className="mx-2 inline-block" />
-          Profile
-        </Link>
-        <Link
-          href="#"
-          className="flex items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
-        >
-          <IoTicketOutline size={20} className="mx-2 inline-block" />
-          Orders
-        </Link>
-        {isAuthenticated && (
-          <button
-            className="flex w-full items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
-            onClick={() => logout()}
-          >
-            <IoLogOutOutline size={20} className="mx-2" />
-            Logout
-          </button>
-        )}
+          {/* User Menu */}
 
-        {!isAuthenticated && (
-          <Link
-            href="/auth/login"
-            className="flex items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
-          >
-            <IoLogInOutline size={20} className="mx-2 inline-block" />
-            Login
-          </Link>
-        )}
+          <div className="mt-8">
+            <h3 className="text-xl font-bold mx-2">Account</h3>
+            <Link
+              href="/profile"
+              className="flex items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
+            >
+              <IoPersonOutline size={20} className="mx-2 inline-block" />
+              Profile
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
+            >
+              <IoTicketOutline size={20} className="mx-2 inline-block" />
+              Orders
+            </Link>
+            {isAuthenticated && (
+              <button
+                className="flex w-full items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
+                onClick={() => logout()}
+              >
+                <IoLogOutOutline size={20} className="mx-2" />
+                Logout
+              </button>
+            )}
 
-        <hr className="my-4" />
+            {!isAuthenticated && (
+              <Link
+                href="/auth/login"
+                className="flex items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
+              >
+                <IoLogInOutline size={20} className="mx-2 inline-block" />
+                Login
+              </Link>
+            )}
+            <hr className="my-4" />
+          </div>
 
-        {/* Secondary Menu */}
-        <Link
-          href="#"
-          className="flex items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
-        >
-          <IoShirtOutline size={20} className="mx-2 inline-block" />
-          Products
-        </Link>
-        <Link
-          href="#"
-          className="flex items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
-        >
-          <IoTicketOutline size={20} className="mx-2 inline-block" />
-          Orders
-        </Link>
-        <Link
-          href="#"
-          className="flex items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
-        >
-          <IoPeopleOutline size={20} className="mx-2 inline-block" />
-          Users
-        </Link>
+          {/* Admin Menu */}
+          {isAdmin && (
+            <div className="mt-8">
+              <h3 className="text-xl font-bold mx-2">Admin</h3>
+              {menuItems.map((item, index) => (
+                <Link
+                  key={index}
+                  href={item.url}
+                  className="flex items-center my-4 p-2 hover:bg-gray-100 rounded-md transition-all"
+                >
+                  {item.icon}
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
       </nav>
     </>
   );
