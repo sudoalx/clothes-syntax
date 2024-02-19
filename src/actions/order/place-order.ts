667c0bde0d2d1cce3002cb9b4907306226a1c738
Alex <alex@alex-dev.me>
2024-02-19 10:55:07 -0600

    Add placeOrder action to handle order placement

---
+++ src/actions/order/place-order.ts
@@ -0,0 +1,147 @@
+"use server";
+import prisma from "@/lib/prisma";
+
+import { auth } from "@/auth.config";
+import type { Address, Size } from "@/interfaces";
+
+interface ProductToOrder {
+  productId: string;
+  quantity: number;
+  size: Size;
+}
+
+export const placeOrder = async (
+  productIds: ProductToOrder[],
+  address: Address
+) => {
+  const session = await auth();
+  const userId = session?.user.id;
+
+  // Verify user session
+  if (!userId) {
+    return {
+      ok: false,
+      message: "No user session",
+    };
+  }
+
+  // Get products information
+  // Note: we can order more than one product with the same id
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map((p) => p.productId),
+      },
+    },
+  });
+
+  // Calculate the total items in order
+  const itemsInOrder = productIds.reduce((count, p) => count + p.quantity, 0);
+
+  // tax, subTotal and total
+  const { subTotal, tax, total } = productIds.reduce(
+    (totals, item) => {
+      const productQuantity = item.quantity;
+      const product = products.find((product) => product.id === item.productId);
+
+      if (!product) throw new Error(`${item.productId} doesn't exist`);
+
+      const subTotal = product.price * productQuantity;
+
+      totals.subTotal += subTotal;
+      totals.tax += subTotal * 0.15;
+      totals.total += subTotal * 1.15;
+
+      return totals;
+    },
+    { subTotal: 0, tax: 0, total: 0 }
+  );
+
+  // Create database transaction
+  try {
+    const prismaTx = await prisma.$transaction(async (tx) => {
+      // 1. Update the stock of the products
+      const updatedProductsPromises = products.map((product) => {
+        //  Get the quantity of the product in the order
+        const productQuantity = productIds
+          .filter((p) => p.productId === product.id)
+          .reduce((acc, item) => item.quantity + acc, 0);
+
+        if (productQuantity === 0) {
+          throw new Error(`${product.id} undefined quantity`);
+        }
+
+        return tx.product.update({
+          where: { id: product.id },
+          data: {
+            inStock: {
+              decrement: productQuantity,
+            },
+          },
+        });
+      });
+
+      const updatedProducts = await Promise.all(updatedProductsPromises);
+
+      // Verify if any product has negative stock
+      // 2. Create the order
+      const order = await tx.order.create({
+        data: {
+          userId: userId,
+          itemsInOrder: itemsInOrder,
+          subTotal: subTotal,
+          tax: tax,
+          total: total,
+
+          OrderItem: {
+            createMany: {
+              data: productIds.map((p) => ({
+                quantity: p.quantity,
+                size: p.size,
+                productId: p.productId,
+                price:
+                  products.find((product) => product.id === p.productId)
+                    ?.price ?? 0,
+              })),
+            },
+          },
+        },
+      });
+
+      // Validate if 0 items in order
+
+      // 3. Create the order address
+      const orderAddress = await tx.orderAddress.create({
+        data: {
+          firstName: address.firstName,
+          lastName: address.lastName,
+          address: address.address,
+          address2: address.address2,
+          zipCode: address.zipCode,
+          city: address.city,
+          phone: address.phone,
+          countryId: address.country,
+          orderId: order.id,
+        },
+      });
+
+      return {
+        updatedProducts: updatedProducts,
+        order: order,
+        orderAddress: orderAddress,
+      };
+    });
+
+    return {
+      ok: true,
+      order: prismaTx.order,
+      prismaTx: prismaTx,
+    };
+  } catch (error: any) {
+    console.error(error);
+    return {
+      ok: false,
+      message: error?.message,
+    };
+  }
+};
