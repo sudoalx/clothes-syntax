4eea2507dd4a190bb498bd721011e01d1b3846dc
Alex <alex@alex-dev.me>
2024-02-19 11:21:38 -0600

    Add authentication check to CheckoutPage

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -2,8 +2,14 @@ import { Title } from "@/components";
 import Link from "next/link";
 import { ProductsInCart } from "./ui/ProductsInCart";
 import { PlaceOrder } from "./ui/PlaceOrder";
+import { auth } from "@/auth.config";
 
-export default function CheckoutPage() {
+export default async function CheckoutPage() {
+  const session = await auth();
+
+  if (!session) {
+    return <h3>You need to be logged in to access this page</h3>;
+  }
   return (
     <div className="flex justify-center items-center mb-28 px10 sm:px-0">
       <div className="flex flex-col w-full ">
