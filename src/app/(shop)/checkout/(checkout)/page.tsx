babdb7c7d164d38e55bd8a9e46b70c9d3d9d3738
Alex <alex@alex-dev.me>
2024-02-19 02:38:39 -0600

    Update checkout page component

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -1,23 +1,6 @@
 import { Title } from "@/components";
-import { initialData } from "@/seed/seed";
-import Image from "next/image";
 import Link from "next/link";
-
-const productsInCart = [
-  initialData.products[0],
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-  initialData.products[3],
-  initialData.products[4],
-  initialData.products[5],
-];
-
-const uniqueProducts = productsInCart.filter(
-  (product, index, self) =>
-    index ===
-    self.findIndex((p) => p.slug === product.slug && p.title === product.title)
-);
+import { ProductsInCart } from "./ui/ProductsInCart";
 
 export default function CheckoutPage() {
   return (
@@ -36,41 +19,7 @@ export default function CheckoutPage() {
             </Link>
 
             {/* Items */}
-            <div className="flex flex-col gap-5 mt-5">
-              {uniqueProducts.map((product) => (
-                <div key={product.slug} className="flex flex-col">
-                  <div className="flex justify-between items-center">
-                    <div className="flex gap-3">
-                      <Image
-                        width={240}
-                        height={240}
-                        src={`/products/${product.images[0]}`}
-                        alt={product.title}
-                        className="w-32 h-32"
-                      />
-                      <div className="flex flex-col">
-                        <span>{product.title}</span>
-                        <span>
-                          ${product.price} x{" "}
-                          {
-                            productsInCart.filter(
-                              (p) => p.slug === product.slug
-                            ).length
-                          }
-                        </span>
-                        <span className="font-bold">
-                          Subtotal: $
-                          {product.price *
-                            productsInCart.filter(
-                              (p) => p.slug === product.slug
-                            ).length}
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
+            <ProductsInCart />
           </div>
 
           {/* Checkout */}
@@ -119,42 +68,17 @@ export default function CheckoutPage() {
 
                   <div className="grid grid-cols-2">
                     <span>Products</span>
-                    <span className="text-right">
-                      {productsInCart.length} items
-                    </span>
+                    <span className="text-right">5 items</span>
                     <span>Subtotal</span>
-                    <span className="text-right">
-                      $
-                      {productsInCart.reduce(
-                        (acc, product) => acc + product.price,
-                        0
-                      )}
-                    </span>
+                    <span className="text-right">$190</span>
                     <span>
                       Taxes <span className="text-sm">(15%)</span>
                     </span>
-                    <span className="text-right">
-                      $
-                      {productsInCart.reduce(
-                        (acc, product) => acc + product.price,
-                        0
-                      ) * 0.15}
-                    </span>
+                    <span className="text-right">$300</span>
                     <span className="text-2xl mt-5">
                       Total <span className="text-sm">(USD)</span>
                     </span>
-                    <span className="text-right text-2xl mt-5">
-                      $
-                      {productsInCart.reduce(
-                        (acc, product) => acc + product.price,
-                        0
-                      ) +
-                        productsInCart.reduce(
-                          (acc, product) => acc + product.price,
-                          0
-                        ) *
-                          0.15}
-                    </span>
+                    <span className="text-right text-2xl mt-5">$212</span>
                   </div>
                 </div>
               </div>
