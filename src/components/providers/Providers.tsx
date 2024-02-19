bf055537a479776ede9e5ad7774665a509448589
Alex <alex@alex-dev.me>
2024-02-19 12:50:35 -0600

    Add PayPalScriptProvider to Providers component

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import { SessionProvider } from "next-auth/react";
 
 interface Props {
@@ -7,5 +8,15 @@ interface Props {
 }
 
 export const Providers = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <PayPalScriptProvider
+      options={{
+        clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID ?? "",
+        intent: "capture",
+        currency: "USD",
+      }}
+    >
+      <SessionProvider>{children}</SessionProvider>
+    </PayPalScriptProvider>
+  );
 };
