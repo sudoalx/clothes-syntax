fffcd5b7c1f6895c28f73ca8b8d65e47e8121028
Alex <alex@alex-dev.me>
2024-02-19 20:45:27 -0600

    Add admin orders page component

---
+++ src/app/(shop)/admin/orders/page.tsx
@@ -0,0 +1,93 @@
+// https://tailwindcomponents.com/component/hoverable-table
+import { getPaginatedOrders } from "@/actions";
+import { Title } from "@/components";
+import clsx from "clsx";
+
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { IoCardOutline } from "react-icons/io5";
+
+export default async function OrdersPage() {
+  const { ok, orders = [] } = await getPaginatedOrders();
+
+  if (!ok) {
+    redirect("/auth/login");
+  }
+
+  return (
+    <>
+      <Title>All orders</Title>
+
+      <div className="mb-10">
+        <table className="min-w-full">
+          <thead className="bg-gray-200 border-b">
+            <tr>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+              >
+                #ID
+              </th>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+              >
+                Full Name
+              </th>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+              >
+                State
+              </th>
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+              >
+                Options
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr
+                key={order.id}
+                className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
+              >
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                  {order.id.slice(0, 6)}
+                </td>
+                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  {order.OrderAddress?.firstName} {order.OrderAddress?.lastName}
+                </td>
+                <td className="flex items-center text-sm  text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  <IoCardOutline
+                    className={clsx(
+                      "text-2xl",
+                      order.isPaid ? "text-green-800" : "text-red-800"
+                    )}
+                  />
+                  {order.isPaid ? (
+                    <span className="mx-2 text-green-800">
+                      Payment completed
+                    </span>
+                  ) : (
+                    <span className="mx-2 text-red-800">Payment pending</span>
+                  )}
+                </td>
+                <td className="text-sm text-gray-900 font-light px-6 ">
+                  <Link
+                    href={`/orders/${order.id}`}
+                    className="hover:underline"
+                  >
+                    View order
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
